922a38d2f20b121b925203cc3c0e9ad135bfd4e2
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-11-02 23:47:47 -0300

    remove unused test

--- test/index.js
+++ test/index.js
@@ -47,21 +47,4 @@ const conductorConfig = {
   }
 }
 
-orchestrator.registerScenario("description of example test", async (s, t) => {
-
-  const { alice, bob } = await s.players({ alice: conductorConfig, bob: conductorConfig })
-
-  // Make a call to a Zome function
-  // indicating the function, and passing it an input
-  const addr = await alice.call("myInstanceName", "my_zome", "create_my_entry", { "entry": { "content": "sample content" } })
-
-  // Wait for all network activity to
-  await s.consistency()
-
-  const result = await alice.call("myInstanceName", "my_zome", "get_my_entry", { "address": addr.Ok })
-
-  // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: ['my_entry', '{"content":"sample content"}'] } })
-})
-
 orchestrator.run()
