bdde92db63e1de1dba3a77c29251074e003e46a1
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-11-02 23:43:25 -0300

    remove unecessary spaces

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@
 const path = require('path')
 const tape = require('tape')
 
-const { Orchestrator, Config, tapeExecutor, singleConductor, combine  } = require('@holochain/try-o-rama')
+const { Orchestrator, Config, tapeExecutor, singleConductor, combine } = require('@holochain/try-o-rama')
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
@@ -48,19 +48,19 @@ const conductorConfig = {
 
 orchestrator.registerScenario("description of example test", async (s, t) => {
 
-  const {alice, bob} = await s.players({alice: conductorConfig, bob: conductorConfig})
+  const { alice, bob } = await s.players({ alice: conductorConfig, bob: conductorConfig })
 
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = await alice.call("myInstanceName", "my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
+  const addr = await alice.call("myInstanceName", "my_zome", "create_my_entry", { "entry": { "content": "sample content" } })
 
   // Wait for all network activity to
   await s.consistency()
 
-  const result = await alice.call("myInstanceName", "my_zome", "get_my_entry", {"address": addr.Ok})
+  const result = await alice.call("myInstanceName", "my_zome", "get_my_entry", { "address": addr.Ok })
 
   // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
+  t.deepEqual(result, { Ok: { App: ['my_entry', '{"content":"sample content"}'] } })
 })
 
 orchestrator.run()
