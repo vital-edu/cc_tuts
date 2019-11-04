f9f9117f8dabed8aa75a011f31aaadc01eefd17b
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-11-03 23:00:24 -0300

    add create_person function

--- zomes/hello/code/src/lib.rs
+++ zomes/hello/code/src/lib.rs
@@ -1,14 +1,31 @@
 #![feature(proc_macro_hygiene)]
+#[macro_use]
 extern crate hdk;
 extern crate hdk_proc_macros;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+#[macro_use]
 extern crate holochain_json_derive;
 
 use hdk::{
+    entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
+};
+
+use hdk::holochain_core_types::{
+    entry::Entry,
+    dna::entry_types::Sharing,
+};
+
+use hdk::holochain_json_api::{
+    json::JsonString,
+    error::JsonError,
+};
+
+use hdk::holochain_persistence_api::{
+    cas::content::Address
 };
 
 use hdk_proc_macros::zome;
@@ -41,11 +58,18 @@ mod hello_zome {
         Ok("Hello Holo".into())
     }
 
+    #[zome_fn("hc_public")]
+    fn create_person(person: Person) -> ZomeApiResult<Address> {
+        let entry = Entry::App("person".into(), person.into());
+        let address = hdk::commit_entry(&entry)?;
+        Ok(address)
+    }
+
     #[entry_def]
     fn person_entry_def() -> ValidatingEntryType {
         entry!(
             name: "person",
-            description "Person to say hello to",
+            description: "Person to say hello to",
             sharing: Sharing::Private,
             validation_package: || {
                 hdk::ValidationPackageDefinition::Entry
