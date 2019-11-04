259b9a2b6be048437bc6600607600c39a0ff659d
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-11-03 22:06:52 -0300

    add test before code it

--- test/index.js
+++ test/index.js
@@ -56,6 +56,23 @@ orchestrator.registerScenario('Test hello holo', async (s, t) => {
 
   t.ok(result.Ok)
   t.deepEqual(result, { Ok: 'Hello Holo' })
+
+  const create_result = await alice.call('cc_tuts', 'hello', 'create_person', {
+    'person': { 'name': 'Alice' }
+  })
+
+  t.ok(create_result.Ok)
+  const alice_person_address = create_result.Ok
+
+  await s.consistency()
+
+  const retrieve_result = await alice.call('cc_tuts', 'hello', 'retrieve_person', {
+    'address': alice_person_address
+  })
+
+  t.ok(retrieve_result.Ok)
+  t.deepEqual(retrieve_result.Ok, { Ok: { 'name': 'Alice' } })
 })
+
 
 orchestrator.run()
